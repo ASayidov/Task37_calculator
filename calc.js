ff226fae6baeef335df7d12e3f8ca12eea41a75a
ASayidov <abdullajonsayidov@gmail.com>
2022-09-25 20:01:04 +0500

    changed js file

--- calc.js
+++ calc.js
@@ -1,39 +1,39 @@
-// let input = document.querySelector("input");
-// let clear_btn = document.getElementById("clear");
+let input = document.querySelector("input");
+let clear_btn = document.getElementById("clear");
 
-// const showNumber = (sym) => {
-//   input.value = input.value + sym;
-// };
+const showNumber = (sym) => {
+  input.value = input.value + sym;
+};
 
-// const calculate = () => {
-//   if (input.value !== "") {
-//     input.value = eval(input.value);
-//   }
-// };
+const calculate = () => {
+  if (input.value !== "") {
+    input.value = eval(input.value);
+  }
+};
 
-// const clear = () => {
-//   input.value = "";
-// };
+const clear = () => {
+  input.value = "";
+};
 
-// clear_btn.addEventListener("click", clear);
+clear_btn.addEventListener("click", clear);
 
 
-// const amallar = (symb) => {
-//   if (input.value!=="") {
-//     input.value = input.value+symb
-//   }
+const amallar = (symb) => {
+  if (input.value!=="") {
+    input.value = input.value+symb
+  }
 
-//   }
+  }
 
-// let bs = document.getElementById("bs")
+let bs = document.getElementById("bs")
 
-// const uchirish = () => {
-// input.value = input.value.slice(0, input.value.length-1)
-//   }
+const uchirish = () => {
+input.value = input.value.slice(0, input.value.length-1)
+  }
 
-// bs.addEventListener("click", uchirish);
+bs.addEventListener("click", uchirish);
 
-let str = '123//'
+// let str = '123//'
 
 // console.log(str.charAt(str.length-1));
 
@@ -42,4 +42,4 @@ let str = '123//'
 
 // }
 
-console.log(str[1]);
+// console.log(str[1]);
